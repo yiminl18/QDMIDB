cc5846370122996b5d21089b26e5bd65a863e11b
yiminl18 <32020422+yiminl18@users.noreply.github.com>
2021-10-26 21:46:04 -0700

    add readme

--- qdmidb/simpledb/src/java/QMIDB/main.java
+++ qdmidb/simpledb/src/java/QMIDB/main.java
@@ -8,18 +8,13 @@ import simpledb.*;
 
 public class main {
     public static void main(String args[]) throws IOException,Exception {
-        //QueryProcessing QP = new QueryProcessing();
+        QueryProcessing QP = new QueryProcessing();
 
-        //System.out.println(Type.MISSING_INTEGER + " "  + Type.NULL_INTEGER);
-        //PredicateSet.print();
-        //Schema.print();
-        //test t = new test();
-        //t.testArray("77,4,,0,2,73569,,1,10,",',');
-        //System.out.println(RelationshipGraph.getLeftJoinAttribute());
-        //t.testSubDesc();
+        test t = new test();
+        t.testComplexQuery();
 
-        DataPreparation dp = new DataPreparation();
-        dp.generateSchema();
+        //DataPreparation dp = new DataPreparation();
+        //dp.generateSchema();
     }
 
 }
