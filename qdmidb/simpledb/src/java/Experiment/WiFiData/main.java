1edb5dfade493067c7d6828b4a2ae9c87a5e3e59
yiminl18 <32020422+yiminl18@users.noreply.github.com>
2021-11-15 17:10:36 -0800

    update jar files for wifi datasets

--- qdmidb/simpledb/src/java/Experiment/WiFiData/main.java
+++ qdmidb/simpledb/src/java/Experiment/WiFiData/main.java
@@ -4,8 +4,8 @@ import Experiment.Service.Metadata;
 
 public class main {
     public static void main(String args[]){
-//        Data d = new Data();
-//        d.writeImputedUser();
+        Data d = new Data();
+        d.writeImputedWiFi();
 //        Metadata md = new Metadata();
 //        md.generateSchema();
     }
